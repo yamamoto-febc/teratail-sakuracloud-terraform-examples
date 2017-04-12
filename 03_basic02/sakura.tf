4c049ba7dddfcea757bb7d56da482baa273aed1c
Kazumichi Yamamoto <yamamoto.febc@gmail.com>
2017-04-12 13:55:41 +0900

    Update sakura.tf
    テスト用パスワード除去

--- 03_basic02/sakura.tf
+++ 03_basic02/sakura.tf
@@ -3,7 +3,6 @@ variable password {
     # 以下任意の値を設定してください。
     # コメントアウトしたままの場合、terraform apply時に入力ダイアログが表示されます。
     # default = "YOUR_PASSWORD_HERE"
-    default = "TestUserPassword01"
 }
 
 # さくらのクラウド APIキーの設定など
