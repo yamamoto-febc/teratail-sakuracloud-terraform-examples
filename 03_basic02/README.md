d44d4e34f75c28a695eda36947625bb1a620b6f9
febc.yamamoto <yamamoto.febc@gmail.com>
2017-04-12 13:51:56 +0900

    入門編2

---
+++ 03_basic02/README.md
@@ -0,0 +1,18 @@
+# クラウドでのインフラ構築を始めよう
+
+入門編2: https://teratail.com/sakura-cloud/tutorial/3
+
+入門編では2回に分けて以下の環境構築を行います。
+
+- 1） 作成済みのサーバ（web-server）にNICを追加
+- 2） スイッチ（192.168.0.0/24セグメント用）の作成
+- 3） 2台目のサーバ（db-server）を新しく作成
+- 4） マップ機能でネットワークを構成
+- 5） グローバル側のサーバ（web-server）のローカル側IPアドレスを設定
+
+入門編2では3)以降を行います。  
+
+![入門編2](https://teratail.storage.googleapis.com/assets/img/sakura/article/2/01.png?1491901812195333 "入門編2")
+
+> 引用元ページ: https://teratail.com/sakura-cloud/tutorial/3
+
--- 03_basic02/sakura.tf
+++ 03_basic02/sakura.tf
@@ -3,6 +3,7 @@ variable password {
     # 以下任意の値を設定してください。
     # コメントアウトしたままの場合、terraform apply時に入力ダイアログが表示されます。
     # default = "YOUR_PASSWORD_HERE"
+    default = "TestUserPassword01"
 }
 
 # さくらのクラウド APIキーの設定など
@@ -45,8 +46,52 @@ resource sakuracloud_server "web-server" {
     name = "web-server"
     disks = ["${sakuracloud_disk.web-server.id}"]
     tags = ["@virtio-net-pci"]
-    # 追加NIC(まだスイッチとは接続していない)
-    additional_interfaces = [""]
+    # 追加NIC(スイッチに接続)
+    additional_interfaces = ["${sakuracloud_switch.switch.id}"]
+
+    # eth1にローカル側IPアドレスを設定(SSHプロビジョニング)
+    connection {
+       user = "root"
+       host = "${self.base_nw_ipaddress}"
+       password = "${var.password}"
+    }
+    provisioner "remote-exec" {
+        inline = [
+          "nmcli connection add type ethernet con-name local-eth1 ifname eth1 ip4 192.168.0.1/24 gw4 192.168.0.254",
+          "nmcli connection up local-eth1"
+        ]
+   }
+}
+
+#------------------------------------------------------------------------------
+# DBサーバー(db-server)の定義
+#------------------------------------------------------------------------------
+# ディスク(20GB/SSD)
+resource sakuracloud_disk "db-server" {
+    name = "db-server"
+    # OSとしてCentOS7.3を指定
+    source_archive_id = "${data.sakuracloud_archive.centos.id}"
+    # サイズ
+    size = 20
+    # SSH or HDD
+    plan = "ssd"
+    # パスワード
+    password = "${var.password}"
+    # ホスト名
+    hostname = "db-server"
+}
+
+# サーバ
+resource sakuracloud_server "db-server" {
+    name = "db-server"
+    disks = ["${sakuracloud_disk.db-server.id}"]
+    tags = ["@virtio-net-pci"]
+    # ローカル接続用のスイッチに接続
+    base_interface = "${sakuracloud_switch.switch.id}"
+    # IPアドレス
+    base_nw_ipaddress = "192.168.0.2"
+    # サブネットマスク長
+    base_nw_mask_len = 24
 }
 
 #------------------------------------------------------------------------------
